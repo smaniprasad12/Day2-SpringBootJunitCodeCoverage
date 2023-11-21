508c16ef97daa53146758e7d0c7fab8a87ef5320
smaniprasad12 <104119726+smaniprasad12@users.noreply.github.com>
2023-11-21 17:03:12 +0530

    Add files via upload

---
+++ AuthenticationUser/src/main/java/com/example/AuthenticationUser/entity/UserCredentials.java
@@ -0,0 +1,27 @@
+package com.example.AuthenticationUser.entity;
+
+	public class UserCredentials {
+	    private String username;
+	    private String password;
+
+	    // Getters and setters
+	    // (You can generate these using your IDE or Lombok library for simplicity)
+	    
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+	
+
+}
